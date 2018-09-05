e82181a39be64c79e42fd3cc45446292c4dfc5c2
Andrey Denisenko <francescogacczi@gmail.com>
2018-09-05 06:46:29 +0300

    changes2

--- laf12-5/HeapTree.cs
+++ laf12-5/HeapTree.cs
@@ -21,8 +21,65 @@ namespace laf12_5
         }
         public bool insert(int key)
         {
-
+            Node nullNode = findNullNode();
+            nullNode.iData = key;
+            trickleUp(nullNode);
+            currentSize++;
             return true;
+        }
+        public Node remove()
+        {
+            if (currentSize == 0)
+                return null;
+
+            int key = root.iData;
+            if (currentSize == 1)
+            {
+                currentSize--;
+                root = null;
+            }
+            else
+            {
+                Node lastNode = findLastNode();
+                root.iData = lastNode.iData;
+                trickleDown(root);
+                currentSize--;
+            }
+            return new Node(key);
+        }
+        private Node findLastNode()
+        {
+            Node parentNode = root;
+            Node lastNode = root;
+
+            int[] path = getPath(currentSize);
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == 0)
+                {
+                    parentNode = lastNode;
+                    lastNode = lastNode.leftChild;
+                    if (i == path.Length - 1)
+                    {
+                        parentNode.leftChild = null;
+                        lastNode.parent = null;
+                        return lastNode;
+                    }
+                }
+                else
+                {
+                    parentNode = lastNode;
+                    lastNode = lastNode.rightChild;
+                    if (i == path.Length - 1)
+                    {
+                        parentNode.rightChild = null;
+                        lastNode.parent = null;
+                        return lastNode;
+                    }
+                }
+            }
+            root = null;
+            return lastNode;
         }
         public void trickleUp(Node currentNode)
         {
@@ -62,6 +119,34 @@ namespace laf12_5
         {
             Node nullNode = new Node();
             int[] path = getPath(currentSize + 1);
+            Node currentNode = root;
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (path[i] == 0)
+                {
+                    if (currentNode.leftChild != null)
+                        currentNode = currentNode.leftChild;
+                    else
+                    {
+                        currentNode.leftChild = nullNode;
+                        nullNode.parent = currentNode;
+                        return nullNode;
+                    }
+                }
+                else
+                {
+                    if (currentNode.rightChild != null)
+                        currentNode = currentNode.rightChild;
+                    else
+                    {
+                        currentNode.rightChild = nullNode;
+                        nullNode.parent = currentNode;
+                        return nullNode;
+                    }
+                }
+            }
+            root = nullNode;
+            return nullNode;
         }
         public int[] getPath(int nodeNumber)
         {
@@ -69,6 +154,77 @@ namespace laf12_5
                 return null;
             int level = (int)(Math.Log(nodeNumber) / Math.Log(2)) + 1;
             int[] binary = new int[level];
+            while (nodeNumber >= 1)
+            {
+                binary[--level] = nodeNumber % 2;
+                nodeNumber /= 2;
+            }
+            int[] path = new int[binary.Length - 1];
+            Array.Copy(binary, 1, path, 0, path.Length);
+            return path;
+        }
+        public void displayHeap()
+        {
+
+            if (root == null)
+            {
+                Console.WriteLine("heapArray: empty heap!");
+                return;
+            }
+            Console.Write("heapArray: "); // array format
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                Node node = queue.Dequeue();
+                Console.Write(node.iData + " ");
+                if (node.leftChild != null)
+                {
+                    queue.Enqueue(node.leftChild);
+                }
+                if (node.rightChild != null)
+                {
+                    queue.Enqueue(node.rightChild);
+                }
+            }
+            Console.WriteLine();
+            int nBlanks = 32;
+            int itemsPerRow = 1;
+            int column = 0;
+            int j = 0; // current item
+            String dots = "...............................";
+            Console.WriteLine(dots + dots); // dotted top line
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                if (column == 0) // first item in row?
+                    for (int k = 0; k < nBlanks; k++)
+                        Console.Write(' ');// preceding blanks
+                Node node = queue.Dequeue(); // Node node = queue.remove();
+                Console.Write(node.iData);
+                if (node.leftChild != null)
+                {
+                    queue.Enqueue(node.leftChild);
+                }
+                if (node.rightChild != null)
+                {
+                    queue.Enqueue(node.rightChild);
+                }
+                if (queue.Count == 0) // done?
+                    break;
+                if (++column == itemsPerRow) // end of row?
+                {
+                    nBlanks /= 2; // half the blanks
+                    itemsPerRow *= 2; // twice the items
+                    column = 0; // start over on
+                    Console.WriteLine(); ; // new row
+                }
+                else
+                    // next item on row
+                    for (int k = 0; k < nBlanks * 2 - 2; k++)
+                        Console.Write(' '); // interim blanks
+            } // end for
+            Console.WriteLine("\n" + dots + dots); // dotted bottom line
         }
     }
 }
--- laf12-5/Program.cs
+++ laf12-5/Program.cs
@@ -8,8 +8,69 @@ namespace laf12_5
 {
     class Program
     {
+        public static string getString()
+        {
+            string str = Console.ReadLine();
+            return str;
+        }
+        public static char getChar()
+        {
+            string str = getString();
+            return str[0];
+        }
+        public static int getInt()
+        {
+            string str = getString();
+            return Int32.Parse(str);
+        }
         static void Main(string[] args)
         {
+            int value;
+            HeapTree theHeap = new HeapTree(); // make a TreeHeap;
+            bool success;
+            theHeap.insert(70);
+            theHeap.insert(40);
+            theHeap.insert(50);
+            theHeap.insert(20);
+            theHeap.insert(60);
+            theHeap.insert(100);
+            theHeap.insert(80);
+            theHeap.insert(30);
+            theHeap.insert(10);
+            theHeap.insert(90);
+            bool b = true;
+            while (b) // until [Ctrl]-[C]
+            {
+                Console.Write("Enter first letter of ");
+                Console.Write("show, insert, remove, quit: ");
+                int choice = getChar();
+                switch (choice)
+                {
+                    case 's': // show
+                        theHeap.displayHeap();
+                        break;
+                    case 'i': // insert
+                        Console.Write("Enter value to insert: ");
+                        value = getInt();
+                        success = theHeap.insert(value);
+                        if (!success)
+                            Console.WriteLine("Can't insert; heap full");
+                        break;
+                    case 'r': // remove
+                        if (!theHeap.isEmpty())
+                            theHeap.remove();
+                        else
+                            Console.WriteLine("Can't remove; heap empty");
+                        break;
+                    case 'q':
+                        b = false;
+                        Console.WriteLine("quit!byebye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid entry\n");
+                        break;
+                } // end switch
+            }
 
             Console.ReadLine();
         }
